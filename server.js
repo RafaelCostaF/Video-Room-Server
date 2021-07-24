bc2691310a431d1f9e5c5b100a980e641a5f7156
RafaelCostaF <57494016+RafaelCostaF@users.noreply.github.com>
2021-07-24 00:06:41 -0300

    Final

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const express = require('express')
 const upload = require('express-fileupload')
 const app = express()
 const server = require('http').Server(app)
-const io = require('socket.io')(server)
 const formidable = require('formidable')
 
 app.set('views','./public')
@@ -19,6 +18,7 @@ app.get('/', (req,res) => {
     res.render('index', {rooms:rooms})
 })
 
+
 app.get('/styles/style.css', (req,res) => {
         fs.readFile(
         path.join(__dirname,'./public/styles','style.css'),
@@ -29,6 +29,7 @@ app.get('/styles/style.css', (req,res) => {
         }
     )
 })
+
 
 app.get('*.css',(req,res) => {
     fs.readFile(
@@ -44,6 +45,7 @@ app.get('*.css',(req,res) => {
     )
 })
 
+
 app.get('*.png',(req,res) => {
     fs.readFile(
         path.join(__dirname,'./public',req.url),
@@ -58,6 +60,7 @@ app.get('*.png',(req,res) => {
     )
 })
 
+
 app.get('*.jpg',(req,res) => {
     fs.readFile(
         path.join(__dirname,'./public',req.url),
@@ -71,6 +74,7 @@ app.get('*.jpg',(req,res) => {
         }
     )
 })
+
 
 app.get("/video", (req,res) => {
 
@@ -140,6 +144,7 @@ app.get('/:room', (req,res) => {
     res.render('room',{ roomName : req.params.room})
 })
 
+
 app.post('/joinroom' , (req,res) => {
     if (rooms[req.body.streamid] == null){
         return res.redirect('/')
@@ -148,106 +153,3 @@ app.post('/joinroom' , (req,res) => {
 })
 
 server.listen(8000)
-
-// io.on('connection', socket => {
-//     socket.on('new-user',(room,name) =>{
-//         socket.join(room)
-//         rooms[room].users[socket.id] = name
-//         socket.to(room).broadcast.emit('user-connected',name)
-//     })
-//     socket.on('disconnect',(room,name) =>{
-//         socket.to(room).broadcast.emit('user-disconnected',name)
-//         delete rooms[room].users[name]
-//     })
-// })
-
-// http.createServer((req,res) => {
-//     const file = req.url === '/' ? 'index.html' : req.url
-//     const filePath = path.join(__dirname,'public',file)
-//     const extname = path.extname(filePath)
-
-//     const allowedFileTypes = ['.html','.css','.js','.png','.jpg']
-//     const allowed = allowedFileTypes.find(item => item == extname)
-
-
-//     if(extname == '.html'){
-//         fs.readFile(
-//             path.join(__dirname,'public',file),
-//             (err,content) => {
-//                 if(err) throw err
-    
-//                 res.end(content)
-//             }
-//         )
-//     }
-
-//     else if(extname == '.css'){
-//         fs.readFile(
-//             path.join(__dirname,'public/styles','style.css'),
-//             (err,content) => {
-//                 if(err) throw err
-
-//                 res.end(content)
-//             }
-//         )
-//     }
-
-//     else if(extname == '.png' || extname == '.jpg'){
-//         fs.readFile(
-//             path.join(__dirname,'./public',file.toString()),
-//             (err,content) => {
-//                 if(err) throw err
-
-//                 res.end(content)
-//             }
-//         )
-//     }
-
-//     else if(file == '/video'){
-//         const range = req.headers.range;
-//   if (!range) {
-//     res.status(400).send("Requires Range header");
-//   }
-
-//   // get video stats (about 61MB)
-//   const videoPath = "bigbuck.mp4";
-//   const videoSize = fs.statSync("bigbuck.mp4").size;
-
-//   // Parse Range
-//   // Example: "bytes=32324-"
-//   const CHUNK_SIZE = 10 ** 6; // 1MB
-//   const start = Number(range.replace(/\D/g, ""));
-//   const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
-
-//   // Create headers
-//   const contentLength = end - start + 1;
-//   const headers = {
-//     "Content-Range": `bytes ${start}-${end}/${videoSize}`,
-//     "Accept-Ranges": "bytes",
-//     "Content-Length": contentLength,
-//     "Content-Type": "video/mp4",
-//   };
-
-//   // HTTP Status 206 for Partial Content
-//   res.writeHead(206, headers);
-
-//   // create video read stream for this particular chunk
-//   const videoStream = fs.createReadStream(videoPath, { start, end });
-
-//   // Stream the video chunk to the client
-//   videoStream.pipe(res);
-//     }
-
-//     else{
-//         fs.readFile(
-//             path.join(__dirname,'public',"index.html"),
-//             (err,content) => {
-//                 if(err) throw err
-    
-//                 res.end(content)
-//             }
-//         )
-//     }
-
-
-// }).listen(8000,() => console.log('Server running on port 8000'))
